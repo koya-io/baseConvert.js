b70390d3af66186eb9e93e4f2f30f97991a290b7
Koya <_@koya.io>
2016-04-30 20:05:14 +0100

    Validation modification

--- baseConvert/baseConvert.js
+++ baseConvert/baseConvert.js
@@ -5,17 +5,19 @@ $.baseConvert = function(convert_val, convert_from, convert_to) {
    if (convert_from <= 36) {
       convert_val = convert_val.toUpperCase();
    }
-   if (convert_from > 64 || convert_to < 2) {
-      error = 1;
+   if (convert_from > 64 || convert_to < 2 || convert_to > 64 || convert_from < 2) {
+      error = "Error, selector out of bounds.";
+   } else if(convert_val.replace(/[^0-9a-zA-Z+/]/g, '').length != convert_val.length) {
+      error = "Error, invalid character.";
    } else {
       $.each((convert_val + "").split(""), function(i, v){
          if(convert_from >= 63) {
             if (convert_from == 63 && v == "/") {
-               error = 1;
+               error = "Error, invalid character.";
             }
          } else {
-            if (convert_vars.indexOf(v) > convert_from) {
-               error = 1;
+            if (convert_vars.indexOf(v) >= convert_from) {
+               error = "Error, invalid character.";
             }
          }
       });
@@ -33,6 +35,7 @@ $.baseConvert = function(convert_val, convert_from, convert_to) {
             }
          });
          while(convert_workingnum > 0) {
+            convert_workingnum = Math.floor(convert_workingnum);
             if(convert_to >= 63) {
                convert_finalnum = convert_vars64[convert_workingnum % convert_to] + "" + convert_finalnum;
             } else {
@@ -43,7 +46,6 @@ $.baseConvert = function(convert_val, convert_from, convert_to) {
          }
       }
       return convert_finalnum;
-      
    }
-   return "Error";
+   return error;
 }
