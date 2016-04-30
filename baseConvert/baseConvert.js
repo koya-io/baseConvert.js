7e874b78672d2a84c35be5012f29592fb3d4b7a7
Koya <_@koya.io>
2016-04-30 22:42:42 +0100

    

--- baseConvert/baseConvert.js
+++ baseConvert/baseConvert.js
@@ -1,51 +1,57 @@
-$.baseConvert = function(convert_val, convert_from, convert_to) {
-   var error = 0;
-   var convert_vars64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-   var convert_vars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-   if (convert_from <= 36) {
-      convert_val = convert_val.toUpperCase();
+$.baseConvert = function(val, from, to) {
+   var error = "";
+   var vars64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+   var vars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+   if (from <= 36) {
+      val = val.toUpperCase();
    }
-   if (convert_from > 64 || convert_to < 2 || convert_to > 64 || convert_from < 2) {
+   if (from > 64 || to < 2 || to > 64 || from < 2) {
       error = "Error, selector out of bounds.";
-   } else if(convert_val.replace(/[^0-9a-zA-Z+/]/g, '').length != convert_val.length) {
+   } else if (val.replace(/[^0-9a-zA-Z+/]/g, '').length != val.length) {
       error = "Error, invalid character.";
    } else {
-      $.each((convert_val + "").split(""), function(i, v){
-         if(convert_from >= 63) {
-            if (convert_from == 63 && v == "/") {
+      $.each((val + "").split(""), function(i, v) {
+         if (from >= 63) {
+            if (from == 63 && v == "/") {
                error = "Error, invalid character.";
             }
          } else {
-            if (convert_vars.indexOf(v) >= convert_from) {
+            if (vars.indexOf(v) >= from) {
                error = "Error, invalid character.";
             }
          }
       });
    }
-   if(error == 0) {
-      if(convert_from >= 2 && convert_from <= 64 && convert_to >= 2 && convert_to <= 64) {
-         var convert_workingnum = "", convert_finalnum = "";
-         convert_workingnum = 0, convert_finalnum = "";
-         convert_val += "";convert_from += "";convert_to += "";
-         $.each(convert_val.split(""), function(i,v){
-            if(convert_from >= 63) {
-               convert_workingnum += (convert_vars64.indexOf(v) * Math.pow(convert_from, (convert_val.length - i - 1)));
-            } else {
-               convert_workingnum += (convert_vars.indexOf(v) * Math.pow(convert_from, (convert_val.length - i - 1)));
-            }
-         });
-         while(convert_workingnum > 0) {
-            convert_workingnum = Math.floor(convert_workingnum);
-            if(convert_to >= 63) {
-               convert_finalnum = convert_vars64[convert_workingnum % convert_to] + "" + convert_finalnum;
-            } else {
-               convert_finalnum = convert_vars[convert_workingnum % convert_to] + "" + convert_finalnum;
-            }
-            convert_workingnum -= convert_workingnum % convert_to;
-            convert_workingnum /= convert_to;
+   if (error == "") {
+      var working = bigInt(0);
+      var final = "";
+      val += "", from += "", to += "";
+      $.each(val.split(""), function(i, v) {
+         if (from >= 63) {
+            var exponent = bigInt(val.length).minus(i).minus(1);
+            var height = bigInt(from).pow(exponent);
+            working = bigInt(vars64.indexOf(v)).multiply(height);
+            working = bigInt(working).add(mediation);
+            console.log(working + " = " + vars64.indexOf(v) + " * " + height + " ^ " + exponent);
+         } else {
+            var exponent = bigInt(val.length).minus(i).minus(1);
+            var height = bigInt(from).pow(exponent);
+            var mediation = bigInt(vars.indexOf(v)).multiply(height);
+            working = bigInt(working).add(mediation);
+            console.log(working + " = " + vars64.indexOf(v) + " * " + height + " ^ " + exponent);
          }
+      });
+      while (working > 0) {
+         if (to >= 63) {
+            final = vars64[bigInt(working).mod(to)] + "" + final;
+         } else {
+            final = vars[bigInt(working).mod(to)] + "" + final;
+         }
+         var mediation2 = bigInt(working).mod(to);
+         bigInt(working).minus(mediation2);
+         working = bigInt(working).divide(to);
       }
-      return convert_finalnum;
+      return final;
    }
    return error;
 }
